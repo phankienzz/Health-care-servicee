bdaf346ccd684b8f8010a0bf333569238a743aed
ThangVu2303 <120199247+ThangVu2303@users.noreply.github.com>
2025-03-04 02:07:02 +0700

    update

--- SWP/src/java/dao/MedicalExaminationDAO.java
+++ SWP/src/java/dao/MedicalExaminationDAO.java
@@ -8,6 +8,8 @@ import context.DBContext;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import model.MedicalExamination;
 import model.Role;
 
@@ -16,6 +18,7 @@ import model.Role;
  * @author Gigabyte
  */
 public class MedicalExaminationDAO extends DBContext {
+
     public MedicalExamination getMedicalExaminationByID(int examinationID) {
         ServiceDAO dao = new ServiceDAO();
         CustomerDAO cusDAO = new CustomerDAO();
@@ -29,7 +32,7 @@ public class MedicalExaminationDAO extends DBContext {
                 return new MedicalExamination(
                         rs.getInt("examinationID"),
                         rs.getString("examinationDate"),
-                        cusDAO.getCustomerByID(rs.getInt("customerID")) ,
+                        cusDAO.getCustomerByID(rs.getInt("customerID")),
                         rs.getString("status"),
                         proDAO.getProfessionalbyID(rs.getInt("consultantID")),
                         rs.getString("notes"),
@@ -41,10 +44,38 @@ public class MedicalExaminationDAO extends DBContext {
         }
         return null;
     }
-    
+
+    public List<MedicalExamination> getAllMedicalExamination() {
+        List<MedicalExamination> medicalExaminationList = new ArrayList<>();
+        ServiceDAO dao = new ServiceDAO();
+        CustomerDAO cusDAO = new CustomerDAO();
+        ProfessionalDAO proDAO = new ProfessionalDAO();
+        String sql = "SELECT * FROM MedicalExamination ";
+
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                medicalExaminationList.add(new MedicalExamination(
+                        rs.getInt("examinationID"),
+                        rs.getString("examinationDate"),
+                        cusDAO.getCustomerByID(rs.getInt("customerID")),
+                        rs.getString("status"),
+                        proDAO.getProfessionalbyID(rs.getInt("consultantID")),
+                        rs.getString("notes"),
+                        rs.getString("createdAt"),
+                        dao.getServiceExaminationID(rs.getInt("examinationID"))));
+
+            }
+
+        } catch (SQLException e) {
+        }
+        return medicalExaminationList; // Trả về danh sách khách hàng
+    }
+
     public static void main(String[] args) {
         MedicalExaminationDAO m = new MedicalExaminationDAO();
-        
-        System.out.println(m.getMedicalExaminationByID(1).getList().get(0).getPackageName());
+
+        System.out.println(m.getAllMedicalExamination().get(0).getList().get(0).getPackageName());
     }
 }
