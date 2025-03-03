deb2e40e44e10604c5a0ca14a63001f3342d2811
ThangVu2303 <120199247+ThangVu2303@users.noreply.github.com>
2025-03-03 21:33:16 +0700

    Update MedicalExamination.java

--- SWP/src/java/model/MedicalExamination.java
+++ SWP/src/java/model/MedicalExamination.java
@@ -14,6 +14,87 @@ public class MedicalExamination {
     private String consultationType;
     private Customer customerId;
     private String status;
-    private Staff consultantId;
+    private Professional consultantId;
+    private String note;
+    private String createdAt;
+
+    public MedicalExamination() {
+    }
+
+    public MedicalExamination(int examinationID, String examinationDate, String consultationType, Customer customerId, String status, Professional consultantId, String note, String createdAt) {
+        this.examinationID = examinationID;
+        this.examinationDate = examinationDate;
+        this.consultationType = consultationType;
+        this.customerId = customerId;
+        this.status = status;
+        this.consultantId = consultantId;
+        this.note = note;
+        this.createdAt = createdAt;
+    }
+
+    public int getExaminationID() {
+        return examinationID;
+    }
+
+    public void setExaminationID(int examinationID) {
+        this.examinationID = examinationID;
+    }
+
+    public String getExaminationDate() {
+        return examinationDate;
+    }
+
+    public void setExaminationDate(String examinationDate) {
+        this.examinationDate = examinationDate;
+    }
+
+    public String getConsultationType() {
+        return consultationType;
+    }
+
+    public void setConsultationType(String consultationType) {
+        this.consultationType = consultationType;
+    }
+
+    public Customer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Customer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Professional getConsultantId() {
+        return consultantId;
+    }
+
+    public void setConsultantId(Professional consultantId) {
+        this.consultantId = consultantId;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+    
     
 }
